a1a556c228a7bf777f5c09d20779d1fbc4e79661
Tatiana Belchik <ttnblchk@gmail.com>
2023-10-23 14:58:30 +0300

    add config file

---
+++ www/Config.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App;
+
+class Config
+{
+    const DB_DRIVER = 'mysql';
+    const USER = 'root';
+    const PASSWORD = 'root';
+    const HOST = 'mysql1'; // '192.168.144.2'
+    const DB_NAME = 'project_db';
+    const CHARSET = 'utf8';
+    const TABLE = 'participants';
+
+    const NOVICE = 'novice';
+    const MAX_SHARES_AMOUNT = 500;
+    const START_DATE_OF_PRESIDENT = 1273449600;
+}
