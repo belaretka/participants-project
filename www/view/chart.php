fac7c3a2efac2eca99a90c09dbe24671037dfeab
Tatiana Belchik <ttnblchk@gmail.com>
2023-11-01 21:56:32 +0300

    add tooltip and icons on chart

--- www/view/chart.php
+++ www/view/chart.php
@@ -26,20 +26,42 @@
 
                     for (const participant of participants) {
                         let row = [];
+
+                        let f = '' + participant["firstname"] + '<div class="icon_container" style="color:#0c0b0b; font-style:bold">';
+
+                        switch (participant["position"]) {
+                            case 'manager':
+                                 f += '<div id="icon"><svg xmlns="http://www.w3.org/2000/svg" class="icon" aria-hidden="true" focusable="false" viewBox="0 0 576 512">   <path d="M320 192h17.1c22.1 38.3 63.5 64 110.9 64c11 0 21.8-1.4 32-4v4 32V480c0 17.7-14.3 32-32 32s-32-14.3-32-32V339.2L280 448h56c17.7 0 32 14.3 32 32s-14.3 32-32 32H192c-53 0-96-43-96-96V192.5c0-16.1-12-29.8-28-31.8l-7.9-1c-17.5-2.2-30-18.2-27.8-35.7s18.2-30 35.7-27.8l7.9 1c48 6 84.1 46.8 84.1 95.3v85.3c34.4-51.7 93.2-85.8 160-85.8zm160 26.5v0c-10 3.5-20.8 5.5-32 5.5c-28.4 0-54-12.4-71.6-32h0c-3.7-4.1-7-8.5-9.9-13.2C357.3 164 352 146.6 352 128v0V32 12 10.7C352 4.8 356.7 .1 362.6 0h.2c3.3 0 6.4 1.6 8.4 4.2l0 .1L384 21.3l27.2 36.3L416 64h64l4.8-6.4L512 21.3 524.8 4.3l0-.1c2-2.6 5.1-4.2 8.4-4.2h.2C539.3 .1 544 4.8 544 10.7V12 32v96c0 17.3-4.6 33.6-12.6 47.6c-11.3 19.8-29.6 35.2-51.4 42.9zM432 128a16 16 0 1 0 -32 0 16 16 0 1 0 32 0zm48 16a16 16 0 1 0 0-32 16 16 0 1 0 0 32z"/>  </svg></div>' + participant["position"] + '</div>'; break;
+                            case 'vice president':
+                                f += '<div id="icon"><svg xmlns="http://www.w3.org/2000/svg" class="icon" aria-hidden="true" focusable="false" viewBox="0 0 576 512">  <path d="M180.5 141.5C219.7 108.5 272.6 80 336 80s116.3 28.5 155.5 61.5c39.1 33 66.9 72.4 81 99.8c4.7 9.2 4.7 20.1 0 29.3c-14.1 27.4-41.9 66.8-81 99.8C452.3 403.5 399.4 432 336 432s-116.3-28.5-155.5-61.5c-16.2-13.7-30.5-28.5-42.7-43.1L48.1 379.6c-12.5 7.3-28.4 5.3-38.7-4.9S-3 348.7 4.2 336.1L50 256 4.2 175.9c-7.2-12.6-5-28.4 5.3-38.6s26.1-12.2 38.7-4.9l89.7 52.3c12.2-14.6 26.5-29.4 42.7-43.1zM448 256a32 32 0 1 0 -64 0 32 32 0 1 0 64 0z"/></svg></div>' + participant["position"] + '</div>'; break;
+                            case 'president':
+                                f += '<div id="icon"><svg xmlns="http://www.w3.org/2000/svg" class="icon" aria-hidden="true" focusable="false" viewBox="0 0 640 512"> <path d="M352 124.5l-51.9-13c-6.5-1.6-11.3-7.1-12-13.8s2.8-13.1 8.7-16.1l40.8-20.4L294.4 28.8c-5.5-4.1-7.8-11.3-5.6-17.9S297.1 0 304 0H416h32 16c30.2 0 58.7 14.2 76.8 38.4l57.6 76.8c6.2 8.3 9.6 18.4 9.6 28.8c0 26.5-21.5 48-48 48H538.5c-17 0-33.3-6.7-45.3-18.7L480 160H448v21.5c0 24.8 12.8 47.9 33.8 61.1l106.6 66.6c32.1 20.1 51.6 55.2 51.6 93.1C640 462.9 590.9 512 530.2 512H496 432 32.3c-3.3 0-6.6-.4-9.6-1.4C13.5 507.8 6 501 2.4 492.1C1 488.7 .2 485.2 0 481.4c-.2-3.7 .3-7.3 1.3-10.7c2.8-9.2 9.6-16.7 18.6-20.4c3-1.2 6.2-2 9.5-2.2L433.3 412c8.3-.7 14.7-7.7 14.7-16.1c0-4.3-1.7-8.4-4.7-11.4l-44.4-44.4c-30-30-46.9-70.7-46.9-113.1V181.5v-57zM512 72.3c0-.1 0-.2 0-.3s0-.2 0-.3v.6zm-1.3 7.4L464.3 68.1c-.2 1.3-.3 2.6-.3 3.9c0 13.3 10.7 24 24 24c10.6 0 19.5-6.8 22.7-16.3zM130.9 116.5c16.3-14.5 40.4-16.2 58.5-4.1l130.6 87V227c0 32.8 8.4 64.8 24 93H112c-6.7 0-12.7-4.2-15-10.4s-.5-13.3 4.6-17.7L171 232.3 18.4 255.8c-7 1.1-13.9-2.6-16.9-9s-1.5-14.1 3.8-18.8L130.9 116.5z"/>  </svg></div>' + participant["position"] + '</div>'; break;
+                            default:
+                                break;
+                        }
+
+                        let info = (participant) => {
+                            let result = '';
+                            for (const [key, value] of Object.entries(participant)) {
+                                result += key + ' : ' + value + '\n\n';
+                                //result += value + '\n';
+                            }
+                            return result;
+                        };
+
                         if (participant["position"] !== 'novice') {
-                            let f = '' + participant["firstname"] + '<div style="color:red; font-style:italic">' + participant["position"] + '</div>';
                             row.push(
                                 {
                                     'v': participant["firstname"],
                                     'f': f
                                 },
                                 participant["parent_id"] === 0 ? '' : participant["parent_firstname"],
-                                '');
+                                info(participant));
                         } else {
                             row.push(
                                 participant["firstname"],
                                 participant["parent_firstname"],
-                                '');
+                                info(participant));
                         }
                         rows.push(row);
                     }
@@ -47,7 +69,7 @@
                     data.addRows(rows);
                     var chart = new google.visualization.OrgChart(document.getElementById('chart_div'));
                     // Draw the chart, setting the allowHtml option to true for the tooltips.
-                    chart.draw(data, {'allowHtml': true});
+                    chart.draw(data, {'tooltip.isHtml': true, 'allowHtml': true});
                 });
         }
 
--- www/view/template/header.php
+++ www/view/template/header.php
@@ -3,7 +3,35 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <link rel="stylesheet" type="text/css" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
+    <link rel="stylesheet" type="text/css"
+          href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
     <script type="text/javascript" src="https://www.gstatic.com/charts/loader.js"></script>
+    <style>
+        .icon {
+        align-content: center;
+        width: 3em;
+        height: 3em;
+        vertical-align: -.125em;
+        }
+
+        svg {
+        fill: #5e60d3
+        }
+
+        .icon_container {
+        display: flex;
+        flex-direction: column-reverse;
+        justify-content: space-around;
+        flex-wrap: wrap;
+        align-items: center;
+        font-weight: bold;
+        font-style: italic;
+        }
+
+        /*div.google-visualization-tooltip {*/
+        /*    border: blue 10px solid;*/
+        /*    background-color: aliceblue;*/
+        /*}*/
+        </style>
 </head>
 
